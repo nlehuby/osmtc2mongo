4b4b9e6b98031e8bad085e39dee46b3463a789a3
Noémie <noemie.lehuby@openmailbox.org>
2017-02-02 20:58:04 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,10 @@ pub struct Coord {
 }
 impl Coord {
     fn new(lat_param: f64, lon_param: f64) -> Coord {
-        Coord { lat: lat_param, lon: lon_param }
+        Coord {
+            lat: lat_param,
+            lon: lon_param,
+        }
     }
 }
 
@@ -80,12 +83,20 @@ pub struct Line {
 /* to_multilinestring is to be used when issue #8 is resolved*/
 impl Route {
     fn to_multilinestring(&self) -> wkt::types::MultiLineString {
-        let wkt_linestrings = self.shape.iter()
-            .map(|vec_coord|
+        let wkt_linestrings = self.shape
+            .iter()
+            .map(|vec_coord| {
                 vec_coord.iter()
-                    .map(|coord| wkt::types::Coord {x: coord.lon, y: coord.lat, z: None, m: None})
+                    .map(|coord| {
+                        wkt::types::Coord {
+                            x: coord.lon,
+                            y: coord.lat,
+                            z: None,
+                            m: None,
+                        }
+                    })
                     .collect()
-            )
+            })
             .map(|wkt_coords| wkt::types::LineString(wkt_coords))
             .collect();
         wkt::types::MultiLineString(wkt_linestrings)
@@ -95,29 +106,25 @@ impl Route {
 impl Encodable for Route {
     fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
         s.emit_struct("Route", 4, |s| {
-            try!(s.emit_struct_field("id", 0, |s| {
-                s.emit_str(&self.id)
-            }));
-            try!(s.emit_struct_field("name", 1, |s| {
-                s.emit_str(&self.name)
-            }));
-            try!(s.emit_struct_field("code", 2, |s| {
-                s.emit_str(&self.code)
-            }));
-            try!(s.emit_struct_field("shape", 3, |s| {
-                if self.shape.len() == 0 {
-                    s.emit_str(&"")
-                } else {
-                    let linestring : String = self.shape.iter()
-                        .map(|vec_coord| vec_coord.iter()
-                            .map(|coord| format!("{} {}", coord.lon.to_string(), coord.lat.to_string()))
+            try!(s.emit_struct_field("id", 0, |s| s.emit_str(&self.id)));
+            try!(s.emit_struct_field("name", 1, |s| s.emit_str(&self.name)));
+            try!(s.emit_struct_field("code", 2, |s| s.emit_str(&self.code)));
+            try!(s.emit_struct_field("shape", 3, |s| if self.shape.len() == 0 {
+                s.emit_str(&"")
+            } else {
+                let linestring: String = self.shape
+                    .iter()
+                    .map(|vec_coord| {
+                        vec_coord.iter()
+                            .map(|coord| {
+                                format!("{} {}", coord.lon.to_string(), coord.lat.to_string())
+                            })
                             .collect::<Vec<String>>()
                             .join(", ")
-                        )
-                        .collect::<Vec<String>>()
-                        .join("), (");
-                    s.emit_str(&format!("MULTILINESTRING(({}))", linestring))
-                }
+                    })
+                    .collect::<Vec<String>>()
+                    .join("), (");
+                s.emit_str(&format!("MULTILINESTRING(({}))", linestring))
             }));
             Ok(())
         })
@@ -147,8 +154,8 @@ fn is_route(obj: &osmpbfreader::OsmObj) -> bool {
 }
 
 fn get_one_coord_from_way(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                 way: &osmpbfreader::objects::Way)
-                 -> Coord {
+                          way: &osmpbfreader::objects::Way)
+                          -> Coord {
     way.nodes
         .iter()
         .filter_map(|node_id| {
@@ -161,25 +168,25 @@ fn get_one_coord_from_way(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::
 }
 
 fn get_one_coord_from_rel(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                 rel: &osmpbfreader::objects::Relation)
-                 -> Coord {
+                          rel: &osmpbfreader::objects::Relation)
+                          -> Coord {
     rel.refs
         .iter()
         .filter_map(|refe| obj_map.get(&refe.member))
-        .filter_map(|osm_obj| {
-            match *osm_obj {
-                Way(ref way) => Some(get_one_coord_from_way(obj_map, way)),
-                Node(ref node) => Some(Coord::new(node.lat(), node.lon())),
-                Relation(..) => None,
-            }
+        .filter_map(|osm_obj| match *osm_obj {
+            Way(ref way) => Some(get_one_coord_from_way(obj_map, way)),
+            Node(ref node) => Some(Coord::new(node.lat(), node.lon())),
+            Relation(..) => None,
         })
         .next()
         .unwrap_or(Coord::new(0., 0.))
 }
 
 fn osm_way_to_vec(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                  osm_way: &osmpbfreader::Way) -> Vec<Coord> {
-    osm_way.nodes.iter()
+                  osm_way: &osmpbfreader::Way)
+                  -> Vec<Coord> {
+    osm_way.nodes
+        .iter()
         .filter_map(|id| obj_map.get(&osmpbfreader::OsmId::Node(*id)))
         .filter_map(|osm_obj| osmpbfreader::OsmObj::node(osm_obj))
         .map(|node| Coord::new(node.lat(), node.lon()))
@@ -187,7 +194,8 @@ fn osm_way_to_vec(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
 }
 
 fn osm_route_to_shape(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                      osm_relation: &osmpbfreader::Relation) -> Vec<Vec<Coord>> {
+                      osm_relation: &osmpbfreader::Relation)
+                      -> Vec<Vec<Coord>> {
     osm_relation.refs
         .iter()
         .filter_map(|refe| obj_map.get(&refe.member))
@@ -212,46 +220,61 @@ fn osm_line_to_routes_list(route_master: &osmpbfreader::Relation) -> Option<Vec<
 }
 
 fn osm_obj_to_route(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                    obj: &osmpbfreader::OsmObj) -> Option<Route> {
+                    obj: &osmpbfreader::OsmObj)
+                    -> Option<Route> {
     match *obj {
-        Relation(ref rel)=> {
-            Some(Route { id: format!("Route:Relation:{}", rel.id.0),
-                         name: rel.tags.get("name").cloned().unwrap_or("".to_string()),
-                         code: rel.tags.get("ref").cloned().unwrap_or("".to_string()),
-                         shape: osm_route_to_shape(obj_map, rel)
-                })
-        },
-        _ => None
+        Relation(ref rel) => {
+            Some(Route {
+                id: format!("Route:Relation:{}", rel.id.0),
+                name: rel.tags.get("name").cloned().unwrap_or("".to_string()),
+                code: rel.tags.get("ref").cloned().unwrap_or("".to_string()),
+                shape: osm_route_to_shape(obj_map, rel),
+            })
+        }
+        _ => None,
     }
 }
 
 fn osm_obj_to_line(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                    obj: &osmpbfreader::OsmObj) -> Option<Line> {
+                   obj: &osmpbfreader::OsmObj)
+                   -> Option<Line> {
     match *obj {
-        Relation(ref rel)=> {
-            Some(Line { id: format!("Line:Relation:{}", rel.id.0),
-                         name: rel.tags.get("name").cloned().unwrap_or("".to_string()),
-                         code: rel.tags.get("ref").cloned().unwrap_or("".to_string()),
-                         shape: osm_route_to_shape(obj_map, rel),
-                         routes_id : osm_line_to_routes_list(rel).unwrap_or(vec![])
-                })
-        },
-        _ => None
+        Relation(ref rel) => {
+            Some(Line {
+                id: format!("Line:Relation:{}", rel.id.0),
+                name: rel.tags.get("name").cloned().unwrap_or("".to_string()),
+                code: rel.tags.get("ref").cloned().unwrap_or("".to_string()),
+                shape: osm_route_to_shape(obj_map, rel),
+                routes_id: osm_line_to_routes_list(rel).unwrap_or(vec![]),
+            })
+        }
+        _ => None,
     }
 }
 
 
 fn osm_obj_to_stop_point(obj_map: &BTreeMap<osmpbfreader::OsmId, osmpbfreader::OsmObj>,
-                        obj: &osmpbfreader::OsmObj)
-                        -> StopPoint {
+                         obj: &osmpbfreader::OsmObj)
+                         -> StopPoint {
     let (obj_type, obj_id, coord) = match *obj {
         Relation(ref rel) => ("Relation", rel.id.0, get_one_coord_from_rel(obj_map, rel)),
         Way(ref way) => ("Way", way.id.0, get_one_coord_from_way(obj_map, way)),
-        Node(ref node) => ("Node", node.id.0, Coord { lat: node.lat(), lon: node.lon() })
+        Node(ref node) => {
+            ("Node",
+             node.id.0,
+             Coord {
+                 lat: node.lat(),
+                 lon: node.lon(),
+             })
+        }
     };
     let name = obj.tags().get("name").cloned().unwrap_or("".to_string());
     let id = format!("StopPoint:{}:{}", obj_type, obj_id);
-    StopPoint { id: id, name: name, coord: coord }
+    StopPoint {
+        id: id,
+        name: name,
+        coord: coord,
+    }
 }
 
 pub fn get_stops_from_osm(pbf: &mut OsmPbfReader) -> Vec<StopPoint> {
@@ -278,7 +301,7 @@ pub fn get_lines_from_osm(pbf: &mut OsmPbfReader) -> Vec<Line> {
         .collect()
 }
 
-pub fn write_stops_to_csv(stops : Vec<StopPoint>) {
+pub fn write_stops_to_csv(stops: Vec<StopPoint>) {
     let csv_file = std::path::Path::new("/tmp/osmtc2mongo.csv");
     let mut wtr = csv::Writer::from_file(csv_file).unwrap();
 
@@ -287,7 +310,7 @@ pub fn write_stops_to_csv(stops : Vec<StopPoint>) {
     }
 }
 
-pub fn write_routes_to_csv(routes : Vec<Route>) {
+pub fn write_routes_to_csv(routes: Vec<Route>) {
     let csv_file = std::path::Path::new("/tmp/osmtc2mongo_routes.csv");
     let mut wtr = csv::Writer::from_file(csv_file).unwrap();
 
@@ -296,19 +319,19 @@ pub fn write_routes_to_csv(routes : Vec<Route>) {
     }
 }
 
-pub fn write_lines_to_csv(lines : Vec<Line>) {
-    let lines_csv_file = std::path::Path::new("osmtc2mongo_lines.csv");
+pub fn write_lines_to_csv(lines: Vec<Line>) {
+    let lines_csv_file = std::path::Path::new("/tmp/osmtc2mongo_lines.csv");
     let mut lines_wtr = csv::Writer::from_file(lines_csv_file).unwrap();
     lines_wtr.encode(("id", "name", "code")).unwrap();
 
-    let csv_file = std::path::Path::new("osmtc2mongo_lines_routes.csv");
+    let csv_file = std::path::Path::new("/tmp/osmtc2mongo_lines_routes.csv");
     let mut wtr = csv::Writer::from_file(csv_file).unwrap();
     wtr.encode(("parent_relation_id", "member_id")).unwrap();
 
     for l in &lines {
         lines_wtr.encode((&l.id, &l.name, &l.code)).unwrap();
-	for r in &l.routes_id {
-		wtr.encode((&l.id, &r)).unwrap();
-	}
+        for r in &l.routes_id {
+            wtr.encode((&l.id, &r)).unwrap();
+        }
     }
 }
