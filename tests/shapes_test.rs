d274d12e9c76937191a84c6389a22fa35d6a3dc2
Nicolas Berard <nicolas.berard@canaltp.fr>
2019-04-05 17:11:29 +0200

    filter out route shape with less than 2 coords

---
+++ tests/shapes_test.rs
@@ -0,0 +1,14 @@
+extern crate osm_transit_extractor;
+extern crate osmpbfreader;
+
+#[test]
+pub fn osm_shapes_filter_way_with_only_one_existing_node() {
+    let osm_path = std::env::current_dir()
+        .unwrap()
+        .join("tests/fixtures/sample-lite.osm.pbf");
+    let mut parsed_pbf = osmpbfreader::OsmPbfReader::new(std::fs::File::open(&osm_path).unwrap());
+    let lines = osm_transit_extractor::get_lines_from_osm(&mut parsed_pbf);
+    let routes = osm_transit_extractor::get_routes_from_osm(&mut parsed_pbf);
+    assert_eq!(lines[0].shape.len(), 1);
+    assert_eq!(routes[0].shape.len(), 1);
+}
